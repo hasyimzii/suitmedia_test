486435f50694d7bdf8126d7bd3f7e9d31677eb5a
hasyimzii <arihasyim.zii@gmail.com>
2022-01-14 17:20:42 +0700

    Update third page

--- lib/views/third_page.dart
+++ lib/views/third_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:suitmedia_test/config/style.dart';
+import 'package:provider/provider.dart';
+import '../providers/username_provider.dart';
+
+import '../config/style.dart';
 
 import '../widgets/app_layout.dart';
+import '../widgets/image_avatar.dart';
 
 class ThirdPage extends StatelessWidget {
   const ThirdPage({Key? key}) : super(key: key);
@@ -10,32 +14,55 @@ class ThirdPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppLayout(
       title: 'Third Screen',
-      body: ListView.separated(
-        itemCount: 10,
-        separatorBuilder: (BuildContext context, int index) {
-          return const Divider(height: 1);
-        },
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: ClipOval(
-              child: Image.network(
-                'https://reqres.in/img/faces/7-image.jpg',
-                width: 49,
-                height: 49,
-                fit: BoxFit.cover,
-              ),
-            ),
-            title: Text(
-              'Firstname Lastname',
-              style: titleListText,
-            ),
-            subtitle: Text(
-              'email@email.com',
-              style: subtitleListText,
-            ),
+      body: Consumer<UsernameProvider>(
+        builder: (
+          BuildContext context,
+          UsernameProvider username,
+          Widget? child,
+        ) {
+          return ListView.separated(
+            itemCount: username.length,
+            separatorBuilder: (BuildContext context, int index) {
+              return const Divider(height: 1);
+            },
+            itemBuilder: (BuildContext context, int index) {
+              String _fullName = '${username.list[index].firstName} ${username.list[index].lastName}';
+              return _listContent(
+                avatar: username.list[index].avatar,
+                fullName: _fullName,
+                email: username.list[index].email,
+                onTap: () {
+                  username.setUsername(_fullName);
+                },
+              );
+            },
           );
         },
       ),
+    );
+  }
+
+  Widget _listContent({
+    required String avatar,
+    required String fullName,
+    required String email,
+    required VoidCallback onTap,
+  }) {
+    return InkWell(
+      child: ListTile(
+        leading: ImageAvatar(
+          url: avatar,
+        ),
+        title: Text(
+          fullName,
+          style: titleListText,
+        ),
+        subtitle: Text(
+          email,
+          style: subtitleListText,
+        ),
+      ),
+      onTap: onTap,
     );
   }
 }
