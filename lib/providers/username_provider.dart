c74e5ec489285d6bdaf75e6c25ec6ae58ba50eb2
hasyimzii <arihasyim.zii@gmail.com>
2022-01-14 17:20:31 +0700

    Update username provider

--- lib/providers/username_provider.dart
+++ lib/providers/username_provider.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/cupertino.dart';
 
+import '../models/user.dart';
+import '../network/user_api.dart';
+
 class UsernameProvider with ChangeNotifier {
+  final List<Datum> _userList = [];
   String _username = 'Selected User Name';
 
+  // get api
+  Future<void> getUser() async {
+    User? user = await UserApi.getUser();
+    if(user == null) {
+      return;
+    } else {
+      _userList.addAll(user.data);
+      notifyListeners();
+    }
+  }
+
+  // get api result
+  List<Datum> get list => _userList;
+  int get length => _userList.length;
+
+  // get username
   String get username => _username;
   
   void setUsername(String username) {
